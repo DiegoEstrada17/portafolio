c99761b762d06630d00eb3161cdd216b2de24024
unknown <iscdiegoestrada@hotmail.com>
2023-07-26 00:18:16 -0600

    Se agregó funcionalidad de botones encriptar y desencriptar

--- index.js
+++ index.js
@@ -24,3 +24,30 @@ else{
     alert("Debes ingresar algun texto");
 }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById ("titulo-mensaje");
+    let enunciado = document.getElementById("enunciado");
+    let joven = document.getElementById("joven"); 
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
+
+    if (texto.length != 0){
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    enunciado.textContent= "";
+    joven.src = "./imagenes/Mu+¦eco.png";
+}
+else{
+    joven.src = "./imagenes/diamod.jpg";
+    tituloMensaje.textContent = "Ningun mensaje fue encriptado";
+    enunciado.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes ingresar algun texto");
+}
+}
